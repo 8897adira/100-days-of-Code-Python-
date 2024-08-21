e4bc3c8d21b90c8a21add5efb2efb73aafd0bb3d
Aditi More <89569578+8897adira@users.noreply.github.com>
2024-08-21 23:08:04 +0530

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,3 @@
+cityname = input("What is the name of the city you grew up in? \n")
+petname = input("What is the name of your pet? \n")
+band_name = print("Your band name could be " + cityname + " " + petname)
---
+++ day 1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+print("Aditi More \nNeha More") 
+
+print("Aditi" + "More")
+
+print("hello" + input("What is your name?")+ "!")
+
+name = input("what is your name?")
+print(len(name))
+
