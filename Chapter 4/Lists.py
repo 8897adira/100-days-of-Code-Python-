300904ef54e1897da0226378514832a643d10472
Aditi More <89569578+8897adira@users.noreply.github.com>
2025-06-09 00:41:06 +0530

    Delete Chapter 4/Lists.py

--- Chapter 4/Lists.py
+++
@@ -1,8 +0,0 @@
-friends = ["apple", "Orange", 5, 3.14, False, "Prachi"]
-print(friends[0])
-
-#changing a value in list
-friends[0] = "mango"
-print(friends[0])
-print(friends[1:4]) #will display items on index 1 2 and 3
-
