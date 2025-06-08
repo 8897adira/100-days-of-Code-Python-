e103e6f2185ffbd02bd896b747b8e3ad53b1a363
Aditi More <89569578+8897adira@users.noreply.github.com>
2025-06-09 00:40:03 +0530

    Create Lists.py

---
+++ Chapter 4/Lists.py
@@ -0,0 +1,8 @@
+friends = ["apple", "Orange", 5, 3.14, False, "Prachi"]
+print(friends[0])
+
+#changing a value in list
+friends[0] = "mango"
+print(friends[0])
+print(friends[1:4]) #will display items on index 1 2 and 3
+
